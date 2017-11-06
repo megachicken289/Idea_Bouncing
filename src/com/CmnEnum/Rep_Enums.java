9ec23c82632f45c1ceef8bf915615e25062f4913
XPTmHQ <megachicken289@me.com>
2017-11-05 20:46:32 -0500

    Created Rep_Enums.java and folder for common enums and arrays

---
+++ src/com/CmnEnum/Rep_Enums.java
@@ -0,0 +1,27 @@
+package com.CmnEnum;
+
+public class Rep_Enums
+{
+    enum colors {
+        red,
+        blue,
+        green,
+        black,
+        yellow
+    }
+
+    enum days {
+        sunday, monday, tuesday, wednesdy,
+        thursday, friday, saturday
+    }
+
+    enum months {
+        january, february, march, april,
+        may, june, july, august, september,
+        october, november, december
+    }
+
+    enum daysInMonths {
+
+    }
+}
