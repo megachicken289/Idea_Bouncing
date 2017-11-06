e083a1afc216500753f68f771789fa8767e0ca28
XPTmHQ <megachicken289@me.com>
2017-11-06 02:26:52 -0500

    Added mord code to Start_AllPossibleCombos

--- src/com/AllPossibleCombos/Start_AllPossCombos.java
+++ src/com/AllPossibleCombos/Start_AllPossCombos.java
@@ -1,6 +1,85 @@
 package com.AllPossibleCombos;
 
+import com.Cmn_Mods.Rep_Mods;
+
+import java.util.Scanner;
+
 public class Start_AllPossCombos
 {
-    
+    Rep_Mods mods = new Rep_Mods();
+
+    public void APC_Setup()
+    {
+        Scanner input = new Scanner(System.in);
+
+//        int[] digitWithOutPlaceholders = new int[];   // add with DigitsWithoutTheirPlace
+
+        System.out.println("How many digits is the combo: ");
+        int howManyDigits = input.nextInt();
+        int[] digitPlaceholders = new int[howManyDigits];
+
+        System.out.println("Do you know any digits (and possibly their place) [Y/N]: ");
+        String userHasDigits = mods.redimeReduceTo(1,"",false,"low");
+        DigitsAndTheirPlace(userHasDigits, digitPlaceholders);
+
+/*// add with DigitsWithoutTheirPlace
+        System.out.printf("Do you know any digits, but not their place [Y/N]: ");
+        String userHasDigWOPlace = mods.redimeReduceTo(1,"", false,"low");
+        DigitsWithoutTheirPlace(userHasDigWOPlace, digitWithOutPlaceholders);
+/**/
+
+        System.out.println("Here is all possible combinations, assuming order is not important");
+        // shows all possible combos,
+
+        System.out.println("Here is all possible permutations, assuming order is not important");
+        // shows all possible perma,
+
+
+    }
+
+    protected void DigitsAndTheirPlace(String userHasDigits, int[] digitPlaceholders)
+    {
+        Scanner input = new Scanner(System.in);
+
+        while (userHasDigits.equals("y")) {
+            System.out.println("What place is the digit: ");
+            int tempDigitPlace = input.nextInt();
+
+            System.out.printf("What is the digit: ");
+            int tempDigitNumb = input.nextInt();
+
+            digitPlaceholders[tempDigitPlace] = tempDigitNumb;
+            mods.qDeb("digitPlaceHolders[" + tempDigitPlace + "]", tempDigitNumb );
+
+            // at the end ; when user enters digit
+            System.out.printf("DO you have any more digits to add [Y/N]: ");
+            userHasDigits = mods.redimeReduceTo(1,"",false,"low");
+        }
+    }
+
+    protected void DigitsWithoutTheirPlace(String userHasDigWOPlace, int[] digitWithOutPlaceholders)
+    {
+        Scanner input = new Scanner(System.in);
+
+        while (userHasDigWOPlace.equals("y")) {
+            int tempCount = 0;
+            System.out.printf("Add digit: ");
+            int addedDigit = input.nextInt();
+            digitWithOutPlaceholders[tempCount] = addedDigit;
+
+            // at the end ; when user enters digit
+            System.out.printf("DO you have any more digits to add [Y/N]: ");
+            userHasDigWOPlace = mods.redimeReduceTo(1,"",false,"low");
+        }
+    }
+
+    protected void Calculate_NCR()
+    {
+
+    }
+
+    protected void Calculate_NPR()
+    {
+
+    }
 }
--- src/test/Main_Testing_Class.java
+++ src/test/Main_Testing_Class.java
@@ -1,3 +1,4 @@
+import com.AllPossibleCombos.Start_AllPossCombos;
 import com.Cmn_Mods.Get_Constants_Vars;
 import com.Data_Validation_Program.DVP_Main;
 import com.Quick_Projects.Martyr2_Projects;
@@ -12,8 +13,11 @@ public class Main_Testing_Class
 		DVP_Main dvpM = new DVP_Main();
 		Martyr2_Projects m2p = new Martyr2_Projects();
 		Get_Constants_Vars consts = new Get_Constants_Vars();
+		Start_AllPossCombos apc = new Start_AllPossCombos();
 		//dvpM.DVP_Setup();
-		
+
+		apc.APC_Setup();
+
 		for (int i = 0; i<consts.getConstArrItems().length; i++) {
 			m2p.Reverse_A_String(consts.getConstArrItems()[i]);
 		}
